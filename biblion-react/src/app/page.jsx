e574e9b23d029f686862768e8c4c7edebe561b17
Ananda Guedes do Ó <anandaguedesdoo@gmail.com>
2023-12-04 19:57:03 -0300

    alterações provisórias

--- biblion-react/src/app/page.jsx
+++ biblion-react/src/app/page.jsx
@@ -8,6 +8,8 @@ async function getData() {
 export default async function Home() {
   const books = await getData();
 
+  console.log(JSON.stringify(books, null, 2))
+
   return (
     <>
       <meta charSet="UTF-8" />
@@ -38,15 +40,12 @@ export default async function Home() {
               <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
             </svg>
           </a>
-          <img src="img/logo.png" alt="logo" className="logo" />
+          <img src="biblion-react\src\app\img\logo.png" alt="logo" className="logo" />
         </div>
       </header>
       <main>
-        <div className="lg:max-w-screen-lg mx-auto">
-          <div className="books grid grid-cols-3 gap-6" />
-          {books.map((book) => (
-              <BooksCards {...book} key={book.id} />
-            ))}
+        <div className="lg:max-w-screen-lg mx-auto books grid grid-cols-3 gap-6 pt-3 pb-3">
+          {books.map((book) => (<BooksCards {...book} key={book.id} />))}
         </div>
       </main>
       <footer className="pt-4 text-white bg-[#debcbe] dark:bg-gray-950">
--- biblion-react/src/components/books.jsx
+++ biblion-react/src/components/books.jsx
@@ -6,27 +6,31 @@ export default function BooksCards({
     value,
     autor,
     editora,
+    image
   }) {
   return  ( 
-    <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-4">{id}
-    <div className="flex justify-between menor">
-      <p>
-        <img className="foto" src="${books.image}" width="100px" />
-      </p>
-      <div className="flex-col">
-        <h3 className="p-2 mt-4 text-lg font-semibold text-gray-700 dark:text-white">{name}
-        </h3>
-        <p className="p-2 text-sm text-gray-500 dark:text-white">
-          <span className="font-bold">Preço:</span>{formatCurrency(value)}
+    <div className="bg-white dark:bg-gray-700 shadow-md rounded-lg p-4">
+      <div className="flex justify-between menor">
+        <p>
+          <img className="foto" src={image} width="100px" />
         </p>
-        <p className="p-2 text-sm text-gray-500 dark:text-white">
-          <span className="font-bold">Autor(a):</span>{autor}
-        </p>
-        <p className="p-2 text-sm text-gray-500 dark:text-white">
-          <span className="font-bold">Editora:</span>{editora}
-        </p>
+        <div className="flex-col">
+          <h3 className="p-2 mt-4 text-lg font-semibold text-gray-700 dark:text-white">{name}
+          </h3>
+          <p className="p-2 text-sm text-gray-500 dark:text-white">
+            <span className="font-bold">ISBN:</span> {id}
+          </p>
+          <p className="p-2 text-sm text-gray-500 dark:text-white">
+            <span className="font-bold">Preço:</span> {formatCurrency(value)}
+          </p>
+          <p className="p-2 text-sm text-gray-500 dark:text-white">
+            <span className="font-bold">Autor(a):</span> {autor}
+          </p>
+          <p className="p-2 text-sm text-gray-500 dark:text-white">
+            <span className="font-bold">Editora:</span> {editora}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
