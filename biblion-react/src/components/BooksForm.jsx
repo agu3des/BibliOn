ceff9fd11716fdb11e1e35be4dc2ae1c8dd75163
Ananda Guedes <anandaguedesdoo@gmail.com>
2023-12-11 15:50:12 -0300

    estilo

--- biblion-react/src/components/BooksForm.jsx
+++ biblion-react/src/components/BooksForm.jsx
@@ -189,7 +189,7 @@ export default function BooksForm() {
                   <div>
                     <button onClick={validate}
                       type="submit"
-                      className="py-3 px-4 inline-flex w-full justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-500 text-white hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition-all text-sm dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800">
+                      className="py-3 px-4 inline-flex w-full justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-[#debcbe] text-black hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition-all text-sm dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800">
                       Enviar
                     </button>
                   </div>
